6ac55a790d6404142adc508de159639424577b8b
Imran Sayed <imranhsayed@gmail.com>
2019-03-27 12:32:31 +0530

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require( 'path' );
+module.exports = {
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		filename: "main.js"
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	}
+
+};
